ea667b4351792fb76840a1756d638b027d4efaa5
glasyjeans4 <82333209+glasyjeans4@users.noreply.github.com>
2022-07-26 07:58:10 -0300

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Main from "./Main";
+
+
+
+function App(){
+    return (
+        <>
+             <NavBar/>
+             
+       </>
+        )
+     
+}
+export default App;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar(){
+    return (
+       
+        <navbar id="navbar">
+            <h1 classname ="titulo" >Titulo de la tienda </h1>
+          
+                <a id="li" href = "#"> link </a>
+                <a id="li" href = "#"> link </a>
+                <a id="li" href = "#"> link </a>
+                <a id="li" href = "#"> link </a>
+            <h2>Las ofertas de la semana</h2>   
+        
+        </navbar>
+        )
+     
+}
+export default NavBar;
